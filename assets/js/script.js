7f896a76e80a9884d727449a7447ae8cffe8735c
LaLacou <108777796+users.noreply.github.com>
2022-08-04 19:18:24 -0700

    fixed the form so that when the user doesn't input anything it returns false

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,20 @@ var tasksToDoEl = document.querySelector('#tasks-to-do');
    var taskFormHandler = function(event) {
 
     event.preventDefault();
+
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput =document.querySelector("select[name='task-type']").value;
   //package up data as an object
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to full out the task form.")
+        return false;
+    }  
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
+    formEl.reset();
+
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
    };
